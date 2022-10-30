b9892ff8a01326b9018ccd54023e2b97ab6aaa9f
Yu Zhang <yuz9@dmserv5.cs.illinois.edu>
2022-10-29 23:58:49 -0500

    add tqdm

--- seetopic.sh
+++ seetopic.sh
@@ -11,22 +11,22 @@ fi
 green=`tput setaf 2`
 reset=`tput sgr0`
 
-echo ${green}===Get PLM Embeddings===${reset}
+echo "${green}===Get PLM Embeddings===${reset}"
 python get_bert_emb.py --dataset $DATASET --model $MODEL
 
-echo ${green}===Iter 0: PLM Module===${reset}
+echo "${green}===Iter 0: PLM Module===${reset}"
 python get_bert_nn.py --dataset $DATASET --model $MODEL --num_iter 0
 
 for i in $(seq $ITER); do
-	echo ${green}===Iter $i: PLM Module===${reset}
+	echo "${green}===Iter $i: PLM Module===${reset}"
 	python get_bert_nn.py --dataset $DATASET --model $MODEL --num_iter $i
 
-	echo ${green}===Iter $i: Local Module===${reset}
+	echo "${green}===Iter $i: Local Module===${reset}"
 	cd seed_guided_emb/
 	./run_local.sh $DATASET $i
 	cd ../
 
-	echo ${green}===Iter $i: Ensemble===${reset}
+	echo "${green}===Iter $i: Ensemble===${reset}"
 	python ensemble.py --dataset $DATASET --num_iter $(($i+1)) --rho $RHO
 done
 
