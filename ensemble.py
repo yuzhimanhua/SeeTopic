78b4173e07d9962ad202d5dbc9b4da1ffd3855c8
Yu Zhang <yuz9@dmserv21.cs.illinois.edu>
2022-12-27 21:56:52 -0600

    ensemble

--- ensemble.py
+++ ensemble.py
@@ -1,4 +1,5 @@
 import argparse
+from collections import defaultdict
 
 parser = argparse.ArgumentParser(description='main', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--dataset', default='scidocs')
@@ -10,7 +11,6 @@ dataset = args.dataset
 num_iter = args.num_iter
 rho = args.rho
 topM = 100
-epsilon = 1e-8
 weights = [0.5, 0.5]
 
 if dataset == 'scidocs':
@@ -24,21 +24,12 @@ for i, model in enumerate(models):
 	with open(f'{dataset}/keywords_{model}_{num_iter}.txt') as fin:
 		for c, line in enumerate(fin):
 			if i == 0:
-				scores[c] = {}
+				scores[c] = defaultdict(float)
 			data = line.strip().split(':')[1].split(',')
 			names.append(data[0])
 			data = data[1:topM]
 			for r, word in enumerate(data):
-				if word not in scores[c]:
-					scores[c][word] = [epsilon for _ in range(len(models))]
-				scores[c][word][i] = weights[i] * 1/(r+1)
-
-for c in scores:
-	for word in scores[c]:
-		total_score = 0
-		for x in scores[c][word]:
-			total_score += x ** rho
-		scores[c][word] = total_score
+				scores[c][word] += weights[i] * ((1/(r+1)) ** rho)
 
 words_sorted = [None for _ in range(len(scores))]
 for c in range(len(scores)):
